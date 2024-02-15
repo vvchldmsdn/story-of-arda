87fd9cc1ae1a5a41d50c03371dfc0b8dacbc6c2d
vvchldmsdn <vvchldmsdn@gmail.com>
2024-02-16 01:47:55 +0900

    feat UI: 로고 적용

--- app/ui/topnav.tsx
+++ app/ui/topnav.tsx
@@ -1,9 +1,13 @@
+import { GlobeIcon } from "../lib/icons";
 import NavLinks from "./nav-links";
 
 export default function Topnav() {
   return (
     <div className="flex flex-row h-full p-2">
       <div className="w-64 mr-4 flex justify-center items-center text-3xl text-eeeeee">
+        <div className="mr-4">
+          <GlobeIcon/>
+        </div>
         Story of Arda
       </div>
       <div className="mr-4 grow flex flex-row gap-16 justify-center items-center text-eeeeee text-xl">
