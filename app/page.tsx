86b39eeb93332b955e671dc9536c187442ec2b1b
vvchldmsdn <vvchldmsdn@gmail.com>
2024-03-06 23:02:23 +0900

    hotfix: custom metadata add

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 import Image from 'next/image'
 import Link from 'next/link';
 import { Metadata } from 'next';
-import Head from 'next/head';
+import { Head } from 'next/document';
 
 export const metadata: Metadata = {
-  title: 'naver-site-verification',
-  description: 'dbdac0d25f4b9daf8ff5944a4ad5b8634643bad3'
+  title: 'lord of the rings',
+  description: '반지의 제왕 세계관의 지역들을 지도를 통해 탐험하세요!',
+  other: {
+    'naver-site-verification': 'dbdac0d25f4b9daf8ff5944a4ad5b8634643bad3',
+  },
 }
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <meta name="naver-site-verification" content="dbdac0d25f4b9daf8ff5944a4ad5b8634643bad3" />
-      </Head>
       <div className='text-eeeeee'>
         <Link href="/home">지도화면으로</Link>
       </div>
