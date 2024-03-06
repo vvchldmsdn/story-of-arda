20d7cc0ef06df3a94f8e5cef4775e0faa4422eb4
vvchldmsdn <vvchldmsdn@gmail.com>
2024-03-06 22:44:17 +0900

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { inter, gugi, notoSansKr } from './lib/fonts';
 import './globals.css';
 import {Providers} from "./providers";
 import Topnav from './ui/topnav';
+import Head from 'next/head';
 
 export const metadata: Metadata = {
   title: 'Story of Arda',
@@ -16,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="naver-site-verification" content="dbdac0d25f4b9daf8ff5944a4ad5b8634643bad3" />
+      </Head>
       <body className={notoSansKr.className}>
         <Providers>
         <div className="flex-col">
