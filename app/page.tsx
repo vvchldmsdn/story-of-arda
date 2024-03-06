30a066a2290b4269f728b9e8510457fdc20763e0
vvchldmsdn <vvchldmsdn@gmail.com>
2024-03-06 23:03:40 +0900

    hotfix: delete next/document import in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link';
 import { Metadata } from 'next';
-import { Head } from 'next/document';
 
 export const metadata: Metadata = {
   title: 'lord of the rings',
