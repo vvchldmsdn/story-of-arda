c4015604ae5e1fde2c5f848bb15b838621915635
vvchldmsdn <vvchldmsdn@gmail.com>
2024-03-07 13:40:20 +0900

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,72 @@
+import { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://story-of-arda.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.9,
+    },
+    {
+      url: 'https://story-of-arda.vercel.app/home/',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+    {
+      url: 'https://story-of-arda.vercel.app/detail/minas-tirith',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://story-of-arda.vercel.app/detail/minas-morgul',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://story-of-arda.vercel.app/detail/isengard',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://story-of-arda.vercel.app/detail/osgiliath',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://story-of-arda.vercel.app/detail/edoras',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: `https://story-of-arda.vercel.app/detail/helm's-deep`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://story-of-arda.vercel.app/detail/arnor',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://story-of-arda.vercel.app/detail/angmar',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://story-of-arda.vercel.app/detail/arthedain',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+  ]
+}
