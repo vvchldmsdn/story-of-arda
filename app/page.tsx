bc37b1dbfbb4a1f53278da62ef03912d143308db
vvchldmsdn <vvchldmsdn@gmail.com>
2024-03-07 13:20:57 +0900

    add google site verification

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export const metadata: Metadata = {
   description: '반지의 제왕 세계관의 지역들을 지도를 통해 탐험하세요!',
   other: {
     'naver-site-verification': 'dbdac0d25f4b9daf8ff5944a4ad5b8634643bad3',
+    'google-site-verification': "mxuFLFqYAsu0HWBjQyTl0Mniim654wy_R7NX_qbI9TY",
   },
 }
 
